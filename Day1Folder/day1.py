4739a898fe323b86526324aa9c4f2bb7e69247c5
Edelynn435 <shinyrhythm435@gmail.com>
2025-12-15 07:43:55 +0000

    Dec 15, 2025, 7:43 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,6 +1,11 @@
 # Write all your codes for Day 1 here.
 # COMMENT out the previous task before going on to the next task
-print("hello from day1")
+# print("hello from day1 I am Edelynn")
+# print("I study at Ngee Ann Secondary School")
+# print("I live in Tampines")
+# print("I am 14 years old")
+# print("If I have $2000, I would travel to Korea")
+
 
 ########################################################################
 # Task 1:
@@ -15,16 +20,57 @@ print("hello from day1")
 ########################################################################
 # Task 3:
 
+# zoo1 = "hedgehog"
+# zoo2 = "chameleon"
+# zoo3 = "crocodile"
+# print(zoo1)
+# print(zoo2)
+# print(zoo3)
+
 
 
 ########################################################################
 # Task 4:
+# var1 = 20
+# var2 = 26
+# var3 = (var1) + (var2)
+# print(var3)
+
+# var1 = 10
+# var2 = 2
+# var3 = (var1) / (var2)
+# print(var3)
+
+# var1 = 10
+# var2 = 2
+# var3 = (var1) * (var2)
+# print(var3)
+
+# var1 = 10
+# var2 = 2
+# var3 = (var1) - (var2)
+# print(var3)
+
+# var1 = 10
+# var2 = 2
+# var3 = (var1) + (var2)
+# print(var3)
+
+
+
+
+
+
 
 
 
 ########################################################################
 # Task 5:
 
+name = "Edelynn"
+food = "noodles"
+print("My name is " + name)
+print("I like to eat " + food)
 
 
 ########################################################################
