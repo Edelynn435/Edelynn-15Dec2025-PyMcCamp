8c9fdecb7e4370e19a509210cee2e54991f83981
Edelynn435 <shinyrhythm435@gmail.com>
2025-12-16 07:27:52 +0000

    Dec 16, 2025, 7:27 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -33,8 +33,8 @@
 # for i in range(1, 101, 2):
 #     print(i)
 
-for i in range(100, 1, -1):
-    print(i)
+# for i in range(100, 1, -1):
+#     print(i)
 
 for i in range(2, 100, 2):
     print(i * 2)
