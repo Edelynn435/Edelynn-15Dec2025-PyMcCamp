05d51a7c861be25cdfe023989d92e90622983f34
Edelynn435 <shinyrhythm435@gmail.com>
2025-12-16 07:09:05 +0000

    Dec 16, 2025, 7:09 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -13,8 +13,10 @@
 # for i in range(5):
 #     print(i)
 
-for i in "Sabrina Carpenter":
-    print(i)
+# for i in "Sabrina Carpenter":
+#     print(i)
+
+
 
 
 ########################################################################
