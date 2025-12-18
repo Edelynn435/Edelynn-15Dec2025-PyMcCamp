57421c44fecb67d41a8e6a7c2f2834d5bd2c961c
Edelynn435 <shinyrhythm435@gmail.com>
2025-12-18 07:26:40 +0000

    Dec 18, 2025, 7:26 AM

--- Day4Folder/day4.py
+++ Day4Folder/day4.py
@@ -4,10 +4,10 @@
 
 ########################################################################
 # Task 1:
-import random
-for i in range(10): 
-    num1 = random.randint(1, 9999)
-    print(num1)
+# import random
+# for i in range(10): 
+#     num1 = random.randint(1, 9999)
+#     print(num1)
 
 
     
