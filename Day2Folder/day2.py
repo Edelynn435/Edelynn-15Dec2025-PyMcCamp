eeae0ada71081110de0bcf5a51f3714b907d8a79
Edelynn435 <shinyrhythm435@gmail.com>
2025-12-18 08:08:33 +0000

    Dec 18, 2025, 8:08 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -64,6 +64,6 @@
 
 ########################################################################
 # Additional exercises
-name = Edelynn 
+name = "Edelynn" 
 age = 14
 print(f"I am {name} and I am {age} years old")
